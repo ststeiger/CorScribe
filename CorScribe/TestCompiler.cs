38fd530f1bab0bb8fc3aaddef2a1c625da19f65f
Me <me@no.spam>
2018-06-04 09:22:02 +0200

    Fix string multi-line string for C#

--- CorScribe/TestCompiler.cs
+++ CorScribe/TestCompiler.cs
@@ -109,65 +109,66 @@ return 5+5;
         // http://stackoverflow.com/questions/2684278/why-does-microsoft-jscript-work-in-the-code-behind-but-not-within-a
         public static object Eval(string vbCode)
         {
-            // MsgBox(vbCode)
             Microsoft.VisualBasic.VBCodeProvider c = new Microsoft.VisualBasic.VBCodeProvider();
-            // Dim icc As System.CodeDom.Compiler.ICodeCompiler = c.CreateCompiler()
+            // System.CodeDom.Compiler.ICodeCompiler icc = c.CreateCompiler();
             System.CodeDom.Compiler.CompilerParameters cp = new System.CodeDom.Compiler.CompilerParameters();
-
+            
             cp.ReferencedAssemblies.Add("System.dll");
-            // cp.ReferencedAssemblies.Add("System.Xml.dll")
-            // cp.ReferencedAssemblies.Add("System.Data.dll")
             
-            
+            // cp.ReferencedAssemblies.Add("System.Xml.dll");
+            // cp.ReferencedAssemblies.Add("System.Data.dll");
             // cp.ReferencedAssemblies.Add("Microsoft.JScript.dll");
-            
             
             // Sample code for adding your own referenced assemblies
             // cp.ReferencedAssemblies.Add("c:\yourProjectDir\bin\YourBaseClass.dll")
             // cp.ReferencedAssemblies.Add("YourBaseclass.dll")
             
-            
             cp.CompilerOptions = "/t:library";
             cp.GenerateInMemory = true;
             System.Text.StringBuilder sb = new System.Text.StringBuilder("");
-            sb.Append("Imports System" + Microsoft.VisualBasic.Constants.vbCrLf);
-            // sb.Append("Imports System.Xml" & vbCrLf)
-            // sb.Append("Imports System.Data" & vbCrLf)
-            // sb.Append("Imports System.Data.SqlClient" & vbCrLf)
-            // sb.Append("Imports System.Math" & vbCrLf)
-            sb.Append("Imports Microsoft.VisualBasic" + Microsoft.VisualBasic.Constants.vbCrLf);
 
-            sb.Append("Namespace MyEvalNamespace_2C04AF0B_8EC8_4D84_AD83_51FFBBBBC8C6BFAC7762_5502_40C9_AE35_A545434012A4528B58FB_641E_4D61_BA15_E64C58CD8CFC  " + Microsoft.VisualBasic.Constants.vbCrLf);
-            sb.Append("Class MyEvalClass " + Microsoft.VisualBasic.Constants.vbCrLf);
+            sb.Append(@"
+Option Strict Off
+Option Explicit Off
+Option Implicit On
 
-            sb.Append("Public Function EvalCode() As Object " + Microsoft.VisualBasic.Constants.vbCrLf);
-            // sb.Append("YourNamespace.YourBaseClass thisObject = New YourNamespace.YourBaseClass()")
-            sb.Append(vbCode + Microsoft.VisualBasic.Constants.vbCrLf);
-            sb.Append("End Function " + Microsoft.VisualBasic.Constants.vbCrLf);
-            sb.Append("End Class " + Microsoft.VisualBasic.Constants.vbCrLf);
-            sb.Append("End Namespace " + Microsoft.VisualBasic.Constants.vbCrLf);
-            // Debug.WriteLine(sb.ToString()) ' look at this to debug your eval string
 
-            // Dim cr As System.CodeDom.Compiler.CompilerResults = icc.CompileAssemblyFromSource(cp, sb.ToString())
+Imports System
+' Imports System.Xml
+' Imports System.Math
+' Imports System.Data
+' Imports System.Data.SqlClient
+' Imports Microsoft.VisualBasic
 
+Namespace MyEvalNamespace_2C04AF0B_8EC8_4D84_AD83_51FFBBBBC8C6BFAC7762_5502_40C9_AE35_A545434012A4528B58FB_641E_4D61_BA15_E64C58CD8CFC  
+    Class MyEvalClass 
+        Public Function EvalCode() As Object
+");
+            
+            sb.Append(vbCode);
+            
+sb.Append(@"
+        End Function
+    End Class 
+End Namespace 
+");
+            
             string code = sb.ToString();
             sb.Clear();
             sb = null;
-
+            
+            
             System.CodeDom.Compiler.CompilerResults cr = c.CompileAssemblyFromSource(cp, code);
-            System.Reflection.Assembly a = cr.CompiledAssembly;
-            object o;
-            System.Reflection.MethodInfo mi;
-            o = a.CreateInstance("MyEvalNamespace_2C04AF0B_8EC8_4D84_AD83_51FFBBBBC8C6BFAC7762_5502_40C9_AE35_A545434012A4528B58FB_641E_4D61_BA15_E64C58CD8CFC.MyEvalClass");
+            System.Reflection.Assembly asm = cr.CompiledAssembly;
+            object o = asm.CreateInstance("MyEvalNamespace_2C04AF0B_8EC8_4D84_AD83_51FFBBBBC8C6BFAC7762_5502_40C9_AE35_A545434012A4528B58FB_641E_4D61_BA15_E64C58CD8CFC.MyEvalClass");
             System.Type t = o.GetType();
-            mi = t.GetMethod("EvalCode");
-            object s;
-            s = mi.Invoke(o, null);
-            return s;
+            System.Reflection.MethodInfo mi  = t.GetMethod("EvalCode");
+            object result = mi.Invoke(o, null);
+            return result;
         }
-
-
+        
+        
     }
-
-
+    
+    
 }
